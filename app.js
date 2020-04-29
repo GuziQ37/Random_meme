a904c606d42039ca0c4743e6e19dd673a77ef0fe
GuziQ37 <wojciak.kuba94@gmail.com>
2020-04-29 22:05:14 +0200

    creating api to get memes

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const btn = document.querySelector("button");
 let number = 1;
 
 const addElement = function(){
+    
     const makeDiv = document.createElement('div');
-    document.body.appendChild(makeDiv);
-    number++;
+    
     makeDiv.textContent = number;
+
+    if(number%5 == 0) {
+        makeDiv.classList.add("circle")
+    } 
+
+    document.body.appendChild(makeDiv);
+    
+    number++;
+    
 }
   
 
@@ -25,3 +34,18 @@ console.log({jeden, dwa, trzy});
 
 // wyświetlanie jako tablica
 console.log([jeden, dwa, trzy]);
+
+
+// przygotować API do generowania memów 
+// pomyśleć czy fetchem czy inną metodą
+// 1. przygotować metodę do pobrania memów przez api
+// 2. wyświetlić mema
+// 3. wyświetlać random mema po kliknięciu w przycisk
+// 4. style
+// 5. refaktor
+// 6. szlif
+
+
+fetch('https://api.imgflip.com/get_memes')
+.then(res => res.json())
+.then(data => console.log(data))
